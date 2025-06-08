9ccbedc1ab47e6b41b21cd6424cdaaa8f37a8cb5
navidgoalpure <navid.goalpure@gmail.com>
2025-06-08 21:17:04 +1000

    connect to a pretraind model

---
+++ lambda/bedrock-handler.ts
@@ -0,0 +1,64 @@
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from '@aws-sdk/client-bedrock-runtime';
+
+export const handler = async (event: any) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  
+  try {
+    const body = JSON.parse(event.body || '{}');
+    console.log('Parsed body:', JSON.stringify(body, null, 2));
+    
+    const prompt = body.prompt || 'Hello, Bedrock!';
+    console.log('Using prompt:', prompt);
+
+    const client = new BedrockRuntimeClient({ region: 'ap-southeast-2' }); // Sydney region
+
+    const command = new InvokeModelCommand({
+      modelId: 'amazon.titan-text-express-v1',
+      contentType: 'application/json',
+      accept: 'application/json',
+      body: JSON.stringify({
+        inputText: prompt,
+        textGenerationConfig: {
+          maxTokenCount: 300,
+          temperature: 0.7,
+          topP: 1,
+          stopSequences: []
+        }
+      }),
+    });
+
+    console.log('Sending request to Bedrock:', JSON.stringify(command.input, null, 2));
+    
+    const response = await client.send(command);
+    console.log('Received response from Bedrock:', JSON.stringify(response, null, 2));
+    
+    const responseBody = JSON.parse(Buffer.from(response.body).toString());
+    console.log('Parsed response body:', JSON.stringify(responseBody, null, 2));
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        response: responseBody.results[0].outputText
+      })
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: error instanceof Error ? error.message : 'An unknown error occurred'
+      })
+    };
+  }
+};
--- lib/bedrock-api-stack.ts
+++ lib/bedrock-api-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class BedrockApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new lambdaNodejs.NodejsFunction(this, 'BedrockHandler', {
+      entry: path.join(__dirname, '../lambda/bedrock-handler.ts'),
+      handler: 'handler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+      timeout: cdk.Duration.seconds(30),
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: 'es2022',
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BedrockApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    cdk.Tags.of(handler).add('bedrock-learning', 'true');
+
+    handler.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['bedrock:InvokeModel'],
+        resources: ['*'], // optionally narrow down to specific models
+      })
+    );
+
+    handler.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'logs:CreateLogGroup',
+          'logs:CreateLogStream',
+          'logs:PutLogEvents'
+        ],
+        resources: ['*']
+      })
+    );
+
+    const api = new apigateway.LambdaRestApi(this, 'BedrockAPI', {
+      handler: handler,
+      proxy: false,
+    });
+
+    cdk.Tags.of(api).add('bedrock-learning', 'true');
+
+    const items = api.root.addResource('chat');
+    items.addMethod('POST'); // POST /chat
   }
 }
